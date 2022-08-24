4006b5029b9d40eda845326b36cbc6b3868ef37d
Leandro Assis dos santos <assissantosleandro@poli.ufrj.br>
2022-08-24 15:18:51 -0300

    opencryptoki inicializado

--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#include <opencryptoki/pkcs11.h>
+#include <errno.h>
+
+int main(int argc, char **argv){
+
+    CK_RV C_Initialize(CK_VOID_PTR pInitArgs);
+    
+    printf("Criptoki ok\n");
+    return 0;
+}
+
