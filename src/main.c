59bc597cc5042da8d8ca702492b71dd214cd508f
Leandro Assis dos santos <assissantosleandro@poli.ufrj.br>
2022-08-25 19:03:18 -0300

    .gitignore

--- src/main.c
+++ src/main.c
@@ -2,9 +2,9 @@
 #include <opencryptoki/pkcs11.h>
 #include <errno.h>
 
+/*
+*/
 int main(int argc, char **argv){
-
-    CK_RV C_Initialize(CK_VOID_PTR pInitArgs);
     
     printf("Criptoki ok\n");
     return 0;
